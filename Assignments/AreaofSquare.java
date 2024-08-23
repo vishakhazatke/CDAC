6cf793c5d402ae10a743c8bdb65dd7b7bb1d3f3a
vishakhazatke <124386400+vishakhazatke@users.noreply.github.com>
2024-08-23 20:11:01 +0530

    Delete Assignments directory

--- Assignments/AreaofSquare.java
+++
@@ -1,7 +0,0 @@
-class AreaofSquare {
-    public static void main(String[] args) {
-        int side = 11;
-        int AOS = side * side;
-        System.out.println("Area of Square is " + AOS);  
-    }
-}
