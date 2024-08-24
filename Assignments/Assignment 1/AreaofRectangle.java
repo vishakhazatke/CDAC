22da5ca9da60234b1b55c084968840c33acb4652
vishakhazatke <124386400+vishakhazatke@users.noreply.github.com>
2024-08-24 22:40:44 +0530

    Add files via upload

---
+++ Assignments/Assignment 1/AreaofRectangle.java
@@ -0,0 +1,8 @@
+class AreaofRectangle {
+    public static void main(String [] args){
+        int length = 12;
+        int width = 20;
+        int AOR = length * width;
+        System.out.println("The Area of Rectangle is " + AOR);
+    }
+}
---
+++ Assignments/Assignment 1/LargestthreeNo.java
@@ -0,0 +1,16 @@
+class LargestthreeNo {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 20;
+        int c = 5;
+        if(a > b && a > c){
+            System.out.println("A is greatest number");
+        }
+        else if (b > a && b > c){
+            System.out.println("B is greatest number");
+        }
+        else {
+            System.out.println("C is greatest number");
+        }
+    }
+}
