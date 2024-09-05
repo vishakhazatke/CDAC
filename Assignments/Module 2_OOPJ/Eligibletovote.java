3b0f8550d9b069564fb4613f1716879deef8d106
vishakhazatke <124386400+vishakhazatke@users.noreply.github.com>
2024-09-05 15:32:30 +0530

    Add files via upload

---
+++ Assignments/Module 2_OOPJ/Eligibletovote.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Eligibletovote {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if(age > 18){
+            System.out.println("Eligible for voting");
+        }
+        else{
+            System.out.println("Not eligible for voting");
+        }
+        sc.close();
+    }
+}
